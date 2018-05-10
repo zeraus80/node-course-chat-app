a23eaa5ee8e98b2e81a2a0b4b7b18c74359abe41
Javier Navarro <jnavarros@psl.com.co>
2018-05-09 19:13:06 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('connected to the server');
-
-    socket.emit('createMessage', {
-        to: 'Antonio',
-        text: 'Hi, this is Javier'
-    });
 });
 
 socket.on('disconnect', function() {
