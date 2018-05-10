22176e5dfebf8501a714d99fc4d38d9c861fabda
Javier Navarro <jnavarros@psl.com.co>
2018-05-09 19:35:18 -0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', newMessage => {
         console.log('createMessage', newMessage);
         io.emit('newMessage', {
@@ -23,6 +35,11 @@ io.on('connection', socket => {
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+/*         socket.broadcast.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        }); */
     });
 
     socket.on('disconnect', () => console.log('client has disconnected from the server'));
