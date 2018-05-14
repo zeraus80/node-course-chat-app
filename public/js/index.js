33bb92a51bd23d01c412d148a04b021b43c837d9
Javier Navarro <jnavarros@psl.com.co>
2018-05-14 17:15:25 -0500

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,18 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
-    console.log('new message', message);
+socket.on('newMessage', function({from, text}) {
+    var li = jQuery('<li></li>');
+    li.text(`${from}: ${text}`);
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', event => {
+    event.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+        
+    });
 });
--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ io.on('connection', socket => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', newMessage => {
+    socket.on('createMessage', (newMessage, callback) => {
         console.log('createMessage', newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+        callback('This is returned from the server');
 /*         socket.broadcast.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
